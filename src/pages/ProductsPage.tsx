e88736fb6d12ebe6d42e1abd57206d2d64541822
Ironboy <ironboy@users.noreply.github.com>
2025-04-13 01:49:43 +0200

    minor fixes

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return <>
     <h2>Our products</h2>
     <img src="/images/products.jpg"></img>
-    <p>Are products are fantastic. We will list them here shortly.</p>
+    <p>Our products are fantastic. We will list them here shortly.</p>
     <LoremIpsum />
   </>;
 }
