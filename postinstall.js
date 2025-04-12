4e087cafefebf14ea502fb9a6d8a307a046db055
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 11:07:15 +0200

    better

--- postinstall.js
+++ postinstall.js
@@ -7,5 +7,6 @@ const baseFolder = dirname.slice(0, lastIndex > 0 ? dirname.slice(0, lastIndex) 
 const packageJsonPath = path.join(baseFolder, 'package.json');
 const packageContents = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-packageContents.scripts = { ...packageContents.scripts, 'react-rapide': 'node react-rapide' };
+packageContents.scripts = { ...packageContents.scripts, 'react-rapide': 'node node_modules/react-rapide' };
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageContents, null, '  '), 'utf-8');
+console.log("HEY MAN");
