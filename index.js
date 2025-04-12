4f90cfad18d13e0a0701e9bb6a9025d66f9655a9
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 13:47:21 +0200

    bolder

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ async function runCommand(command) {
   postDo.replacePublic && replaceFolder(baseDir, remoteBaseDir, 'public');
   postDo.replaceIndex && replaceFile(baseDir, remoteBaseDir, 'index.html');
   postDo.patchPackage && patchPackage(baseDir, remoteBaseDir, postDo.patchPackage);
-  log(c.green(postDo.message));
+  log(c.green(c.bold(postDo.message)));
 };
 
 function replaceFolder(target, org, ...folderName) {
