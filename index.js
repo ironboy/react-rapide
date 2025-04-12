81aca8defe119a8a25c15adbb372084dbf259b00
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 09:45:22 +0200

    test

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-// react-rapide, ironboy 2025
+// react-rapide, ironboy 2025, app
 import fs from 'fs';
 import path from 'path';
 import { getFolderOfBranch } from './helpers.js';
 const dirname = import.meta.dirname;
 const tempDir = path.join(dirname, 'temp');
 
-async function start() {
-  let ok = await getFolderOfBranch(tempDir, 'ironboy', 'react-rapide', 'app');
-  ok && await import(path.join(tempDir, 'index.js'));
-}
-
-process.on('exit', () => {
-  fs.rmSync(tempDir, { recursive: true, force: true });
-  fs.mkdirSync(tempDir);
-  fs.writeFileSync(path.join(tempDir, '_is_temp.txt'),
-    'This is a temp dir neeeded by the app.', 'utf-8');
-});
-
-start();
+console.log("HELLO THERE");
