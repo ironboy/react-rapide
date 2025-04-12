ec9530183d8288e9832afa322b508bb445545a58
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 12:33:05 +0200

    more help

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import path from 'path';
 import c from 'chalk';
 import { getBranches, getReadMeOfBranch, getFolderOfBranch } from './helpers.js';
 
-const log = (...x) => console.log(...x);
+const log = (...x) => log(...x);
 const dirname = import.meta.dirname;
 const tempDir = path.join(dirname, '..');
-const arg = process.argv.slice(2)[0] || 'help';
+const arg = process.argv.slice(2)[0] || 'helpFast';
 const commandBranches = await getBranches('ironboy', 'react-rapide', (x) => x.startsWith('command-'));
 const commands = commandBranches.map(x => x.split('command-')[1]);
 const commandsToDisplay = commands.map(x => 'npm run react-rapide ' + x);
@@ -16,17 +16,30 @@ log('');
 await runCommand(arg);
 log('');
 
-function help() {
-  log(c.green(c.bold(("REACT RAPIDE"))));
+function helpFast() {
   log(c.blue(c.bold(('Available commands:'))));
-  log(c.bold(commandsToDisplay.join('\n')));
+  log(c.bold(['npm run react-rapide-help', ...commandsToDisplay].join('\n')));
+  log();
+  log(c.bold(c.green('For more info run ') + 'npm run react-rapide help'));
+}
+
+async function help() {
+  log(c.blue(c.bold(('Available commands:'))));
+  let readMes = {};
+  let disp = [...commandsToDisplay];
+  for (let branch of commandBranches) {
+    log(c.bold(disp.shift()));
+    log('  ' + (await getReadMeOfBranch('ironboy', 'react-rapide', branch)));
+  }
 }
 
 async function runCommand(command) {
-  if (command === 'help') { help(); return; }
+  log(c.green(c.bold(("REACT RAPIDE"))));
+  if (command === 'helpFast') { helpFast(); return; }
+  if (command === 'help') { await help(); return; }
   let index = commands.indexOf(command);
   if (index < 0) {
-    console.log(c.red(c.bold('No such command: ' + command)));
+    log(c.red(c.bold('No such command: ' + command)));
   }
   let branch = commandBranches[index];
   let ok = await getFolderOfBranch(tempDir, 'ironboy', 'react-rapide', branch);
