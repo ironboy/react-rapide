b4f695418deb76704a8a3bd87d04d94580592a11
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 12:11:44 +0200

    test

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 // react-rapide, ironboy 2025, app
 import fs from 'fs';
 import path from 'path';
-import chalk from 'chalk';
+import c from 'chalk';
 import { getBranches, getReadMeOfBranch, getFolderOfBranch } from './helpers.js';
 
 const log = (...x) => console.log(...x);
 const dirname = import.meta.dirname;
 const tempDir = path.join(dirname, '..');
-const arg = process.argv.slice(2)[0];
+const arg = process.argv.slice(2)[0] || 'help';
 const commandBranches = await getBranches('ironboy', 'react-rapide', (x) => x.startsWith('command-'));
-const commands = commandBranches.map(x => 'npm run react-rapide ' + x.split('command-')[1]);
+const commands = commandBranches.map(x => x.split('command-')[1]);
+const commandsToDisplay = commands.map(x => 'npm run react-rapide ' + x);
 
 log('');
-help();
+await runCommand(arg);
 log('');
 
 function help() {
-  log(chalk.green("REACT RAPIDE"));
-  log(chalk.blue('Available commands:'));
-  log(chalk.bold(commands.join('\n')));
+  log(c.green(c.bold(("REACT RAPIDE"))));
+  log(c.blue(c.bold(('Available commands:'))));
+  log(c.bold(commandsToDisplay.join('\n')));
+}
+
+async function runCommand(command) {
+  if (command === 'help') { help(); return; }
+  let index = commands.indexOf(command);
+  if (index < 0) {
+    console.log(c.red(c.bold('No such command: ' + command)));
+  }
+  let branch = commandBranches[index];
+  getFolderOfBranch(tempDir, 'ironboy', 'react-rapide', branch);
+  await import(path.join(tempDir, 'react-rapide-' + branch, 'z-rapide.js'));
 }
