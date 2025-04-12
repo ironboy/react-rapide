a5e6314489e7a01037e9566b5b4893d8e28c7501
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 18:25:19 +0200

    ok example now

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -3,6 +3,7 @@ import LoremIpsum from "react-lorem-ipsum";
 export default function AboutPage() {
   return <>
     <h2>Our products</h2>
+    <img src="/images/products.jpg"></img>
     <p>Are products are fantastic. We will list them here shortly.</p>
     <LoremIpsum p={2} random={true} />
   </>;
