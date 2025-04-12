faeaaa5753b9aae3fdbc85230e6c78bd6574b045
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 09:54:55 +0200

    paths

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // react-rapide, ironboy 2025, app
 import fs from 'fs';
 import path from 'path';
-import { getFolderOfBranch } from './helpers.js';
+import { getBranches, getReadMeOfBranch, getFolderOfBranch } from './helpers.js';
 const dirname = import.meta.dirname;
-const tempDir = path.join(dirname, 'temp');
+const tempDir = path.join(dirname, '..', 'temp');
 
-console.log("HELLO THERE");
+console.log("HELLO THERE", tempDir);
