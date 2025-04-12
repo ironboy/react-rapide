b50a8b8b44526133fe89f0f1cc0d4d21169b78a2
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 12:17:58 +0200

    more

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function runCommand(command) {
     console.log(c.red(c.bold('No such command: ' + command)));
   }
   let branch = commandBranches[index];
-  getFolderOfBranch(tempDir, 'ironboy', 'react-rapide', branch);
+  let ok = getFolderOfBranch(tempDir, 'ironboy', 'react-rapide', branch);
+  console.log('ok', ok);
   await import(path.join(tempDir, 'react-rapide-' + branch, 'z-rapide.js'));
 }
