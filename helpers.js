b3b1a32f733ac9d3bc9dd57b0a0e677227906592
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 10:35:26 +0200

    fine

--- helpers.js
+++ helpers.js
@@ -1,3 +1,6 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 import * as cheerio from 'cheerio';
 import AdmZip from 'adm-zip';
 
@@ -29,7 +32,23 @@ export async function getFolderOfBranch(folderPath, gitHubUser, repository, bran
   try {
     const data = Buffer.from(await (await fetch(url)).arrayBuffer());
     new AdmZip(data).extractAllTo(folderPath, true);
+    packageDiffPatch(path.join(folderPath, repository + '-' + branch));
   }
-  catch (_e) { return false; }
+  catch (_e) { console.log(_e); return false; }
   return true;
+}
+
+function packageDiffPatch(packageJsonFolder) {
+  // Note: Patchning our package json!
+  let ourPackageJsonPath = path.join(import.meta.dirname, 'package.json');
+  let one = JSON.parse(fs.readFileSync(ourPackageJsonPath, 'utf-8'));
+  let two = JSON.parse(fs.readFileSync(path.join(packageJsonFolder, 'package.json'), 'utf-8'));
+  let oldOneDeps = JSON.stringify(one.dependencies);
+  let oldOneDevDeps = JSON.stringify(one.devDependencies);
+  one.dependencies = { ...(one.dependencies || {}), ...(two.dependencies || {}) };
+  one.devDependencies = { ...(one.devDependencies || {}), ...(two.devdDependencies || {}) };
+  fs.writeFileSync(ourPackageJsonPath, JSON.stringify(one, null, '  '));
+  let changes = oldOneDeps !== JSON.stringify(one.dependencies)
+    || oldOneDevDeps !== JSON.stringify(one.devDependencies);
+  changes && execSync('npm install');
 }
--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
-
 import { getBranches, getReadMeOfBranch, getFolderOfBranch } from './helpers.js';
+
+const log = (...x) => console.log(x);
 const dirname = import.meta.dirname;
 const tempDir = path.join(dirname, '..');
 const arg = process.argv.slice(2)[0];
 
-console.log(chalk.green("HELLO THERE"), tempDir);
+log(chalk.green("REACT RAPIDE"), tempDir);
+log(chalk.blue('Available commands:'));
