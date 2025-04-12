b1855aaf89c0259970bca4037a452ae5e8f0ec9d
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 10:35:48 +0200

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path';
 import chalk from 'chalk';
 import { getBranches, getReadMeOfBranch, getFolderOfBranch } from './helpers.js';
 
-const log = (...x) => console.log(x);
+const log = (...x) => console.log(...x);
 const dirname = import.meta.dirname;
 const tempDir = path.join(dirname, '..');
 const arg = process.argv.slice(2)[0];
