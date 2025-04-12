12af4509d80678b04b1fad485530a81c3c97d7cb
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 10:28:26 +0200

    mygod

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // react-rapide, ironboy 2025, app
 import fs from 'fs';
 import path from 'path';
+import chalk from 'chalk',
+
 import { getBranches, getReadMeOfBranch, getFolderOfBranch } from './helpers.js';
 const dirname = import.meta.dirname;
 const tempDir = path.join(dirname, '..');
