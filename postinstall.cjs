fc96e987f3cbe6ba7254972378155b7f1331ae4e
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 18:35:49 +0200

    rr short

--- postinstall.cjs
+++ postinstall.cjs
@@ -6,5 +6,9 @@ const lastIndex = dirname.lastIndexOf('node_modules');
 const baseFolder = dirname.slice(0, lastIndex > 0 ? lastIndex : Infinity);
 const packageJsonPath = path.join(baseFolder, 'package.json');
 const packageContents = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-packageContents.scripts = { ...packageContents.scripts, 'react-rapide': 'node node_modules/react-rapide' };
+packageContents.scripts = {
+  ...packageContents.scripts,
+  'react-rapide': 'node node_modules/react-rapide',
+  'rr': 'node node_modules/react-rapide'
+};
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageContents, null, '  '), 'utf-8');
