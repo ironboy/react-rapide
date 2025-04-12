9133ac91dd709caa441d0c3ad0f9e4708da408fb
Ironboy <ironboy@users.noreply.github.com>
2025-04-12 11:43:36 +0200

    finally

--- postinstall.cjs
+++ postinstall.cjs
@@ -1,9 +1,9 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
-const dirname = import.meta.dirname;
+const dirname = __dirname;
 const lastIndex = dirname.lastIndexOf('node_modules');
-const baseFolder = dirname.slice(0, lastIndex > 0 ? dirname.slice(0, lastIndex) : dirname);
+const baseFolder = dirname.slice(0, lastIndex > 0 ? lastIndex : Infinity);
 const packageJsonPath = path.join(baseFolder, 'package.json');
 const packageContents = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 packageContents.scripts = { ...packageContents.scripts, 'react-rapide': 'node node_modules/react-rapide' };
