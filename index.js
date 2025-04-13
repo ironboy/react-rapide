e3a87174d03b6667760f77a9613f77fd0dbcddff
Ironboy <ironboy@users.noreply.github.com>
2025-04-13 13:23:49 +0200

    bug

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ async function runCommand(command) {
 
   // Now write the real main (see above)
   if (mainContent) {
-    fs.writeFileSync(path.join(baseDir, 'src', 'mainREAL.tsx'), mainContent, 'utf-8');
-    fs.writeFileSync(path.join(undoFolder, 'src', 'main.tsx'), oldMainContent, 'utf-8');
+    fs.writeFileSync(path.join(baseDir, 'src', 'main.tsx'), mainContent, 'utf-8');
+    fs.writeFileSync(path.join(undoFolder, 'src', 'mainREAL.tsx'), oldMainContent, 'utf-8');
   }
 
 
