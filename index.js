c1ba02a3f383318eecb87df61e3276dd73fb2557
Ironboy <ironboy@users.noreply.github.com>
2025-04-14 12:09:37 +0200

    back to basics

--- index.js
+++ index.js
@@ -97,10 +97,6 @@ async function runCommand(command) {
   let baseDir = dirname.slice(0, dirname.lastIndexOf('node_modules'));
   while (baseDir.endsWith('/') || baseDir.endsWith('\\')) { baseDir = baseDir.slice(0, -1); }
   let remoteBaseDir = path.join(tempDir, 'react-rapide-' + branch);
-  // Does not work in Windows with psf-paths, changing to relative path handling for import
-  // let func = (await import('../react-rapide-' + branch + '/z-rapide.js')).default;
-  // CHANGED BACK IFFY BEHAVIOR OTERWIS TELL PEOPLE RUNNING psf-paths it won't work instead
-  // Doesn't work with Vite either! TODO!
   let func = (await import(path.join(remoteBaseDir, 'z-rapide.js'))).default;
 
   let result = func() || {};
