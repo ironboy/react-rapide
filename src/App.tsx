bacf22f77ffb7f99d03a2f20546c7f0f45ad4d95
Ironboy <ironboy@users.noreply.github.com>
2025-05-01 13:36:08 +0200

    good

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 export default function App() {
 
-  const animals = ['rabbit', 'cat', 'dog', 'snake'];
+  // An array of objects is a very common data structure
+  const animals = [
+    { species: 'rabbit', description: 'fluffy' },
+    { species: 'snake', description: 'smart' },
+    { species: 'dog', description: 'loyal' },
+    { species: 'cat', description: 'cheeky' }
+  ];
 
+  // Se what happens if you comment out the line with filter
+  //  and/or the line with sort below!
   return <>
-    <h1>Animals I like</h1>
+    <h1>Animals I like...</h1>
     {animals
-      .filter(animal => animal !== 'snake')
-      .sort((a, b) => a > b ? 1 : -1)
-      .map((animal, i) => <p key={i}>I like {animal}s!</p>)}
+      .filter(({ species }) => species !== 'snake')
+      .sort((a, b) => a.species > b.species ? 1 : -1)
+      .map(({ species, description }, i) => <section key={i}>
+        <img src={'/images/' + species + 's.jpg'} />
+        <h2>{species.slice(0, 1).toUpperCase() + species.slice(1)}s</h2>
+        <p>I like {species}s. They are so {description}!</p>
+      </section>)}
   </>;
 }
