53deceadc74ecc97caffef9cfcb4d624b69e4a05
Ironboy <ironboy@users.noreply.github.com>
2025-05-01 12:35:49 +0200

    better tscript

--- src/parts/LoremIpsum.tsx
+++ src/parts/LoremIpsum.tsx
@@ -1,11 +1,11 @@
-import { loremIpsum } from "lorem-ipsum";
+import { loremIpsum, ILoremIpsumParams } from "lorem-ipsum";
 
-export default function LoremIpsum() {
-  const ipsum = loremIpsum({
-    count: 2,
-    units: 'paragraphs',
-    paragraphLowerBound: 2,
-    paragraphUpperBound: 4
-  }).split('\n');
+export default function LoremIpsum(props: ILoremIpsumParams = {
+  count: 2,
+  units: 'paragraphs',
+  paragraphLowerBound: 2,
+  paragraphUpperBound: 4
+}) {
+  const ipsum = loremIpsum(props).split('\n');
   return <>{ipsum.map((para, i) => <p key={i}>{para}</p>)}</>;
 }
