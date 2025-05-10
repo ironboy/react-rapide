db3dba6b0105db2a0d8147bb0d3852bb3f652c97
Ironboy <ironboy@users.noreply.github.com>
2025-05-10 14:23:34 +0200

    fix

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { JSX, createElement } from 'react';
 // page components
 import AboutPage from './pages/AboutPage.tsx';
+import NotFoundPage from './pages/NotFoundPage.tsx';
 import ProductsPage from './pages/ProductsPage.tsx';
 import StartPage from './pages/StartPage.tsx';
 
@@ -14,6 +15,7 @@ interface Route {
 
 export default [
   AboutPage,
+  NotFoundPage,
   ProductsPage,
   StartPage
 ]
