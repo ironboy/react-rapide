520f7da86248eff0a217769a1da7763c5a16f505
Ironboy <ironboy@users.noreply.github.com>
2025-05-10 11:59:38 +0200

    fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,16 @@
-// pages/routes
+import { JSX } from 'react';
 import StartPage from './pages/StartPage';
 import AboutPage from './pages/AboutPage';
 import ProductsPage from './pages/ProductsPage';
-import { ReactElement } from 'react';
 
-interface RouteWithMenuLabel {
-  path: string,
-  element: ReactElement,
-  menuLabel?: string;
+interface Route {
+  element: JSX.Element;
+  path: string;
+  menuLabel: string;
 }
 
-const routes: RouteWithMenuLabel[] = [
+
+const routes: Route[] = [
   { element: <StartPage />, path: '/', menuLabel: 'Start' },
   { element: <AboutPage />, path: '/about-us', menuLabel: 'About us' },
   { element: <ProductsPage />, path: '/products', menuLabel: 'Our products' }
