648ae6b5aa91ea37d6792905d76c54c71fa20809
Ironboy <ironboy@users.noreply.github.com>
2025-05-10 14:25:35 +0200

    fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,17 +2,19 @@ import { JSX } from 'react';
 import StartPage from './pages/StartPage';
 import AboutPage from './pages/AboutPage';
 import ProductsPage from './pages/ProductsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 interface Route {
   element: JSX.Element;
   path: string;
-  menuLabel: string;
+  menuLabel?: string;
 }
 
 const routes: Route[] = [
   { element: <StartPage />, path: '/', menuLabel: 'Start' },
   { element: <AboutPage />, path: '/about-us', menuLabel: 'About us' },
-  { element: <ProductsPage />, path: '/products', menuLabel: 'Our products' }
+  { element: <ProductsPage />, path: '/products', menuLabel: 'Our products' },
+  { element: <NotFoundPage />, path: '*' }
 ];
 
 export default routes;
