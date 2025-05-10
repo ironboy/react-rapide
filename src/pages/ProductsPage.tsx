20bb2fc587de67b3bca1ff015c3ad44a8e86be37
Ironboy <ironboy@users.noreply.github.com>
2025-05-10 18:34:45 +0200

    fix

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -3,7 +3,7 @@ import LoremIpsum from '../parts/LoremIpsum';
 export default function ProductsPage() {
   return <>
     <h2>Our products</h2>
-    <img src="/images/products.jpg"></img>
+    <img src="/images/products.jpg" />
     <p>Our products are fantastic. We will list them here shortly.</p>
     <LoremIpsum />
   </>;
--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -3,7 +3,7 @@ import LoremIpsum from '../parts/LoremIpsum';
 export default function StartPage() {
   return <>
     <h2>Start</h2>
-    <img src="/images/start.jpg"></img>
+    <img src="/images/start.jpg" />
     <p>This is our start page. Here we say: "Welcome"!</p>
     <LoremIpsum />
   </>;
