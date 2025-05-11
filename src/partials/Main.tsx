0f413ed11a7a376e22618c7c7c71c046925d8b51
Ironboy <ironboy@users.noreply.github.com>
2025-05-11 09:10:24 +0200

    updates

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { useStateObject } from '../utils/useStateObject';
-import { categories, sortDescriptions }
-  from '../utils/productPageHelpers';
 
 export default function Main() {
   // a state to use with outlet context
   const stateAndSetter = useStateObject({
-    categoryChoice: categories[0],
-    sortChoice: sortDescriptions[0],
+    categoryChoice: 'All',
+    sortChoice: 'Price (low to high)',
     bwImages: false
   });
 
