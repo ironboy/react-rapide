32674f37889877e408f63f67e26ec2201e869058
Ironboy <ironboy@users.noreply.github.com>
2025-05-12 00:07:11 +0200

    alt texts

--- src/pages/OurVisionPage.tsx
+++ src/pages/OurVisionPage.tsx
@@ -10,7 +10,10 @@ OurVisionPage.route = {
 export default function OurVisionPage() {
   return <>
     <h2>We run with our vision!</h2>
-    <Image src="/images/start.jpg" />
+    <Image
+      src="/images/start.jpg"
+      alt="A runner's legs and hands at the starting line of a track race."
+    />
     <p>This is a page about our vision. Here we describe it in detail.</p>
     <LoremIpsum />
   </>;
