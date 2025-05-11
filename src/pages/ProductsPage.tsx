b0b7c0feafaf49ed04eee4fb39c458925c770d80
Ironboy <ironboy@users.noreply.github.com>
2025-05-11 23:54:41 +0200

    alt texts

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -9,7 +9,7 @@ ProductsPage.route = {
 export default function ProductsPage() {
   return <>
     <h2>Our products</h2>
-    <img src="/images/products.jpg" />
+    <img src="/images/products.jpg" alt="Cold-pressed juice next to slices of pineapple and mango." />
     <p>Our products are fantastic. We will list them here shortly.</p>
     <LoremIpsum />
   </>;
