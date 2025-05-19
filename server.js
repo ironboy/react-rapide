2a2de608868dd894bc9936428ec7035ef1f3f2b3
Ironboy <ironboy@users.noreply.github.com>
2025-05-19 05:51:03 +0200

    hm

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ import path from 'path';
 import nodemon from 'nodemon';
 
 export default function createServer(type = 'dev') {
-  let rrFolder = import.meta.dirname;
-  rrFolder = rrFolder.slice(0, rrFolder.lastIndexOf('temp'));
-  let serverFolderPath = path.join(rrFolder, 'server');
-  fs.rmSync(serverFolderPath, { recursive: true, force: true });
-  fs.mkdir(serverFolderPath);
-  let serverPath = path.join(rrFolder, 'server', 'server2.js');
-  fs.cpSync(path.join(import.meta.dirname, 'server2.js', serverPath));
-  nodemon({
-    script: serverPath,
-    watch: serverFolderPath
-  });
+  try {
+    let rrFolder = import.meta.dirname;
+    rrFolder = rrFolder.slice(0, rrFolder.lastIndexOf('temp'));
+    let serverFolderPath = path.join(rrFolder, 'server');
+    fs.rmSync(serverFolderPath, { recursive: true, force: true });
+    fs.mkdir(serverFolderPath);
+    let serverPath = path.join(rrFolder, 'server', 'server2.js');
+    fs.cpSync(path.join(import.meta.dirname, 'server2.js', serverPath));
+    nodemon({
+      script: serverPath,
+      watch: serverFolderPath
+    });
+  } catch (e) { console.log(e); }
 }
