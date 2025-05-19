eb43ed82aff3199d506a4f2e35be7df3ca1addd7
Ironboy <ironboy@users.noreply.github.com>
2025-05-19 03:16:07 +0200

    test

--- server.js
+++ server.js
@@ -8,19 +8,22 @@ import {
 } from 'worker_threads';
 
 export default async function createServer(type = 'dev') {
-  const baseDir = import.meta.dirname.split('node_modules')[0];
-  let worker = startWorker();
-  chokidar.watch(
-    path.join(baseDir, '_rapide_run.txt'),
-    { ignoreInitial: true }
-  ).on('all', (event, path) => {
-    if (event === 'add') {
-      worker.postMessage('exit');
-    }
-    if (event === 'unlink') {
-      worker = startWorker();
-    }
-  });
+  try {
+    const baseDir = import.meta.dirname.split('node_modules')[0];
+    let worker = startWorker();
+    chokidar.watch(
+      path.join(baseDir, '_rapide_run.txt'),
+      { ignoreInitial: true }
+    ).on('all', (event) => {
+      if (event === 'add') {
+        worker.postMessage('exit');
+      }
+      if (event === 'unlink') {
+        worker = startWorker();
+      }
+    });
+  }
+  catch (e) { console.log(e); }
 }
 
 function startWorker() {
