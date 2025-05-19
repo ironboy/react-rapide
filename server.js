5c361fd68279dec559c2a43759c0119fa2f06300
Ironboy <ironboy@users.noreply.github.com>
2025-05-19 05:51:28 +0200

    again

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ export default function createServer(type = 'dev') {
     rrFolder = rrFolder.slice(0, rrFolder.lastIndexOf('temp'));
     let serverFolderPath = path.join(rrFolder, 'server');
     fs.rmSync(serverFolderPath, { recursive: true, force: true });
-    fs.mkdir(serverFolderPath);
+    fs.mkdirSync(serverFolderPath);
     let serverPath = path.join(rrFolder, 'server', 'server2.js');
     fs.cpSync(path.join(import.meta.dirname, 'server2.js', serverPath));
     nodemon({
