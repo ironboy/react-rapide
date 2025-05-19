57f47bb53650c3f5585a25283179a7bac813b37b
Ironboy <ironboy@users.noreply.github.com>
2025-05-19 06:15:00 +0200

    test

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ export default function createServer(type = 'dev') {
     let serverPath = path.join(rrFolder, 'server', 'server2.js');
     fs.cpSync(path.join(import.meta.dirname, 'server2.js'), serverPath);
     nodemon({
+      ignoreRoot: [],
       script: serverPath,
       watch: [serverFolderPath],
-      ext: 'js'
+      ext: 'js',
     });
     setTimeout(() => console.log("FOLDER TO WATCH", serverFolderPath), 1000);
   } catch (e) { console.log(e); }
