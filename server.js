2dd8767e7d8d2c0be7dc50365287b0dca72fc7cb
Ironboy <ironboy@users.noreply.github.com>
2025-05-19 03:07:18 +0200

    test

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import path from 'path';
+import chokidar from 'chokidar';
 import {
   Worker,
   // isMainThread,
   // parentPort,
   // workerData,
 } from 'worker_threads';
+
+chokidar.watch(path.join(import.meta.dirname), '_rapide_run.txt', { ignoreInitial: true }).on('all', (event, path) => {
+  console.log("YEAH");
+});
 
 export default async function createServer(type = 'dev') {
   const worker = new Worker(path.join(import.meta.dirname, 'serverWorker.js'), {
