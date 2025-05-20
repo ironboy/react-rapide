316bb2e0485adf370a52ae29abb34d3a58a69fd2
Ironboy <ironboy@users.noreply.github.com>
2025-05-20 13:22:24 +0200

    cleanup

--- server2.js
+++ server2.js
@@ -1,12 +1,2 @@
-import url from 'url';
-import fs from 'fs';
-import path from 'path';
-import { isFreePort } from 'find-free-ports';
-import express from 'express';
-import {
-  createServer as createViteServer,
-  version as viteVersion
-} from 'vite';
-import c from 'chalk';
 import { createServer } from './server3.js';
 createServer('dev');
