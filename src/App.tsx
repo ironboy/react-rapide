f2c0c364b53334eff7d7401f73d809bb215bcca2
Ironboy <ironboy@users.noreply.github.com>
2025-05-26 00:10:24 +0200

    okaish

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
+import { useState } from 'react';
 import useFetchJsonArray from './utils/useFetchJson';
 import Animal from './Animal';
-import Note from './Note';
 
 export interface AnimalData {
   species: string;
   description: string;
+  category: string;
 }
 
 export default function App() {
 
-  // Fetch the animlas from an url returning json
+  // State variables
+  const [filterCategory, setFilterCategory] = useState('All');
+  const [sortOrder, setSortOrder] = useState('a-z');
+
+  // Fetch the animals from an url returning json
   const animals = useFetchJsonArray<AnimalData[]>('/json/animals.json');
 
-  // Se what happens if you comment out the line with filter
-  //  and/or the line with sort below!
   return animals && <>
     <h1>Animals I like...</h1>
-    <Note />
+    <label>
+      <select onChange={e => setFilterCategory(e.target.value)}>
+        {['All', 'Mammals', 'Reptilia'].map(x => <option>{x}</option>)}
+      </select>
+    </label>
+    <label>
+      <select onChange={e => setSortOrder(e.target.value)}>
+        {['a-z', 'z-a'].map(x => <option>{x}</option>)}
+      </select>
+    </label>
     {animals
-      .filter(({ species }) => species !== 'snake')
-      .sort((a, b) => a.species > b.species ? 1 : -1)
+      .filter(({ category }) =>
+        filterCategory === 'All' || category === filterCategory)
+      .sort((a, b) => (sortOrder === 'a-z' ? 1 : -1)
+        * (a.species > b.species ? 1 : -1))
       .map((props, i) => <Animal key={i} {...props} />)
     }
   </>;
--- src/Note.tsx
+++
@@ -1,8 +0,0 @@
-export default function Note() {
-  return <p>
-    <b>Note: </b><br />
-    Now we have 4 components in our React app:<br />
-    <i>App</i>, <i>Note</i>, <i>Animal</i> and <i>SpeciesPhoto</i>. <br />
-    We load the animal data from a JSON url, <br /> using our custom hook <i>useFetchJson</i>.
-  </p>;
-}
