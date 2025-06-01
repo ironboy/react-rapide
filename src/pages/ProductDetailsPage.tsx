0faa8e107d1c3b3525f71e36b71a2600acc682d7
Ironboy <ironboy@users.noreply.github.com>
2025-06-01 11:36:59 +0200

    fix

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,5 +1,6 @@
 import type Product from '../interfaces/Product';
 import { useParams, Link, useLoaderData } from 'react-router-dom';
+import NotFoundPage from './NotFoundPage';
 import Image from '../parts/Image';
 import productsLoader from '../utils/productsLoader';
 
@@ -10,12 +11,21 @@ ProductDetailsPage.route = {
 };
 
 export default function ProductDetailsPage() {
+
   // read the slug from the route parameter :slug
   const { slug } = useParams();
+
   // find the correct product based on its slug
-  const { id, name, quantity, price$, description } =
-    (useLoaderData().products as Product[])
-      .find(x => x.slug === slug) as Product;
+  const product = (useLoaderData().products as Product[])
+    .find(x => x.slug === slug) as Product;
+
+  // if no product found, show 404
+  if (!product) {
+    return <NotFoundPage />;
+  }
+
+  // destructure the properties of the product
+  const { id, name, quantity, price$, description } = product;
 
   return <article className="product">
     <h2>{name}</h2>
