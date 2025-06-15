32eba5172284a6b3f73225e566df990db90fc724
Ironboy <ironboy@users.noreply.github.com>
2025-06-16 01:14:54 +0200

    imports first

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import Server from './classes/Server.js';
 
 const templateDbPath = path.join(import.meta.dirname, 'databases', 'template.sqlite3');
 const liveDbPath = path.join(import.meta.dirname, 'databases', 'live.sqlite3');
 fs.existsSync(liveDbPath) || fs.copyFileSync(templateDbPath, liveDbPath);
-
-import Server from './classes/Server.js';
 
 if (process.argv[2] === 'standalone') {
   new Server();
