a046b6dcf478df096db7a2e2633002bea93e1d05
Ironboy <ironboy@users.noreply.github.com>
2025-06-17 23:36:02 +0200

    bugfix

--- src/parts/Image.tsx
+++ src/parts/Image.tsx
@@ -3,13 +3,14 @@ import { useStateContext } from '../utils/useStateObject';
 // an image component that automatically switches to black and white
 // by adding the css class 'bw' if bwImages is true in our context
 export default function Image(props: any) {
+  console.log(useStateContext());
   const [{ bwImages }] = useStateContext() || [{}];
   props = { ...props };
   props.className =
     // Bootstrap specific classes
     'img-fluid w-100 border border-1 border-primary rounded mb-3'
     // Class for black and white
-    + (bwImages ? ' bw' : '')
+    + (bwImages ? ' bw ' : '')
     // Classes from props
     + (props.className || '');
   return <img {...props} />;
