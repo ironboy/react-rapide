634cb0894e6675d73eab852ef99c59c7572171f8
Ironboy <ironboy@users.noreply.github.com>
2025-06-18 03:30:54 +0200

    ok

--- src/utils/addToCart.ts
+++ src/utils/addToCart.ts
@@ -1,11 +1,12 @@
 export default async function addToCart(
   { productId = 0, setState = () => { }, relative = true, quantity = 1 }: any
 ) {
-  await fetch('/api/change-product-in-cart', {
+  const newCartState = await (await fetch('/api/change-product-in-cart', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ productId, quantity, add: relative })
-  });
+  })).json();
 
   setState('showShoppingCart', true);
+  return newCartState;
 }
