9239489862f50f7cd9f36ebd6d58413b6a3bdcd5
Ironboy <ironboy@users.noreply.github.com>
2025-06-23 13:31:53 +0200

    again

--- helpers.js
+++ helpers.js
@@ -38,6 +38,7 @@ export async function getBranches(gitHubUser, repository, filter = () => true, t
 }
 
 export async function getReadMeOfBranch(gitHubUser, repository, branch, token) {
+  console.log("DEBUGGING getReadMe", branch, "token", token);
   const headers = token ? { 'Authorization': `token ${token}` } : {};
   let variants = ['README.md', 'Readme.md', 'readme.md'];
   const url = `https://raw.githubusercontent.com/${gitHubUser}/${repository}/refs/heads/${branch}/`;
