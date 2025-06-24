32ce30f1d12ec92ea2627cb290133916ee54c6d4
Ironboy <ironboy@users.noreply.github.com>
2025-06-24 08:03:46 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ export default function App() {
     <hr />
     <h2>README FOR REZA:</h2>
     <pre>
-      Hi Reza!
-
-      TO DO:
-      * This should install a little differenlty than other examples, not replacing the source folder or public folder entirely... rather just giving you the files you need.
+      Hi Reza!<br />
+      <br />
+      TO DO:<br />
+      * This should install a little differenlty than other examples, not replacing the source folder or public folder entirely... rather just giving you the files you need.<br />
       * Integration of the data (currently JSON files only) into a backend similar to the one in our REST-api / shopping cart examples to be used in latter stages of the assignment.
-
+      <br /><br />
       /Thomas
     </pre>
   </>;
