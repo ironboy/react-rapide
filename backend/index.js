638b73ba36d9fa033cc5c6be121b94e92ebf2c07
Ironboy <ironboy@users.noreply.github.com>
2025-09-10 02:48:18 +0200

    seems to be working

--- backend/index.js
+++ backend/index.js
@@ -2,35 +2,51 @@ import fs from 'fs';
 import path from 'path';
 import { spawn } from 'child_process';
 import proxy from 'express-http-proxy';
-import killPort from 'kill-port';
+import { isFreePort } from 'find-free-ports';
 
 export default function startBackend(app) {
+
+  let message = `<pre>
+    In order to see something meaningful here
+    run <b>npm run build</b> that compiles/builds
+    your Vite-based project to the dist folder first.
+
+    Then you can see the production version of the app
+    served from the dist folder here.
+  </pre>`;
 
   // Create the dist folder if it does not exist
   const distFolder = path.join(import.meta.dirname, '..', 'dist');
   fs.existsSync(distFolder) || (
     fs.mkdirSync(distFolder),
-    fs.writeFileSync(path.join(distFolder, 'index.html'), 'Hej', 'utf-8')
+    fs.writeFileSync(path.join(distFolder, 'index.html'), message, 'utf-8')
   );
 
   // Calculate db path
   const dbPath = path.join(import.meta.dirname, '_db.sqlite3');
 
   // Start .NET backend from Node.js
-  // (and send distFolder/frontendFolder + dbPath as arguments to it)
   setTimeout(async () => {
-    // await killPort(5001, 'tcp').catch(_e => { });
-    spawn(
-      `dotnet run "${distFolder}" "${dbPath}"`,
+    let startPort = 5001;
+    while (!await isFreePort(startPort)) { startPort++; }
+    let backendProcess = spawn(
+      `dotnet run ${startPort} "${distFolder}" "${dbPath}"`,
       { cwd: import.meta.dirname, stdio: 'inherit', shell: true }
     );
-  }, 1);
+    // Proxy traffic to the backend if the request starts with /api
+    app.use('/api', proxy(`localhost:${startPort}`, {
+      proxyReqPathResolver(req) {
+        return '/api' + req.url;
+      }
+    }));
 
-  // Proxy traffic to the backend if teh request starts with /api
-  app.use('/api', proxy('localhost:5001', {
-    proxyReqPathResolver(req) {
-      return '/api' + req.url;
-    }
-  }));
+    setTimeout(() => {
+      console.log(
+        'Started C#/.NET based Minimal API\n' +
+        '\nNote:\nStill visit the Vite Dev Port for all requests,\n' +
+        'unless you want to check a build,\n' +
+        `in that case visit the server port (${startPort}) directly.`);
+    }, 3000);
+  }, 1);
 
 }
--- backend/src/App.cs
+++ backend/src/App.cs
@@ -3,12 +3,12 @@ Globals = Obj(new
 {
     debugOn = true,
     detailedAclDebug = false,
-    aclOn = true,
+    aclOn = false,
     isSpa = true,
-    port = 5001,
+    port = args[0],
     serverName = "Minimal API Backend",
-    frontendPath = args[0],
-    dbPath = args[1],
+    frontendPath = args[1],
+    dbPath = args[2],
     sessionLifeTimeHours = 2
 });
 
